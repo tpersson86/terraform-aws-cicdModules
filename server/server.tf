95ef59bddba1b4eb5740f2bd7e99cfad6a1f9715
Thomas Persson <tap145@P-IT-HJLDZH3.localdomain>
2022-03-22 16:43:54 -0400

    updating instance size

--- server/server.tf
+++ server/server.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   count                  = var.num_webs
   ami                    = data.aws_ami.ubuntu_16_04.image_id
-  instance_type          = "t2.small"
+  instance_type          = "t2.micro"
   subnet_id              = var.subnet_id
   vpc_security_group_ids = var.vpc_security_group_ids
 
