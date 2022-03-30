93273ef228b75a6af0b7b31ea9be6717a03e9991
Thomas Persson <tap145@P-IT-HJLDZH3.localdomain>
2022-03-30 17:07:07 -0400

    updating tags in data lookup

--- server/data.tf
+++ server/data.tf
@@ -2,9 +2,16 @@ data "aws_ami" "ubuntu_16_04" {
   most_recent = true
 
   filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+    // name   = "name"
+    // values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+    name = "tag:environment"
+    values = ["production"]
   }
 
-  owners = ["099720109477"]
+  filter {
+    name = "tag:version"
+    values = ["3"]
+  }
+
+  owners = ["157252224841"]
 }
